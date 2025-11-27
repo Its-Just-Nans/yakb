a3988cdb5d58b0b1d9779639565cd9ded07c6b38
n4n5 <its.just.n4n5@gmail.com>
2025-11-27 15:02:44 -0700

    uptd

--- src/utils.ts
+++ src/utils.ts
@@ -2,20 +2,22 @@ import { existsSync, mkdirSync, writeFileSync } from "node:fs";
 import { basename, dirname, join } from "node:path";
 
 export const download = async (outputDirectory: string, files: { [ke: string]: string }) => {
-    Object.entries(files).map(async ([url, output]) => {
-        const outputDir = join(outputDirectory, dirname(output));
-        if (!existsSync(outputDir)) {
-            mkdirSync(outputDir);
-        }
-        const outputFile = join(outputDir, basename(output));
-        if (!existsSync(outputFile)) {
-            const file = await fetch(url).catch((e) => {
-                throw e;
-            });
-            const content = await file.arrayBuffer().catch((e) => {
-                throw e;
-            });
-            writeFileSync(outputFile, Buffer.from(content));
-        }
-    });
+    return Promise.all(
+        Object.entries(files).map(async ([url, output]) => {
+            const outputDir = join(outputDirectory, dirname(output));
+            if (!existsSync(outputDir)) {
+                mkdirSync(outputDir);
+            }
+            const outputFile = join(outputDir, basename(output));
+            if (!existsSync(outputFile)) {
+                const file = await fetch(url).catch((e) => {
+                    throw e;
+                });
+                const content = await file.arrayBuffer().catch((e) => {
+                    throw e;
+                });
+                writeFileSync(outputFile, Buffer.from(content));
+            }
+        })
+    );
 };
