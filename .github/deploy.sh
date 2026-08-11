c9858a389c6c09c7782be63d5929b616d7c5c359
n4n5 <its.just.n4n5@gmail.com>
2026-08-11 14:02:30 +0200

    use local trunk

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -15,7 +15,11 @@ rustup target add wasm32-unknown-unknown
 wget -qO- https://github.com/thedodd/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 
 cd yakb-rs
-trunk build --release --public-url "./"
+if [ -f "trunk" ]; then
+    ./trunk build --release --public-url "./"
+else
+    trunk build --release --public-url "./"
+fi
 cd ..
 mv yakb-rs/dist/* dist/animations/
 
