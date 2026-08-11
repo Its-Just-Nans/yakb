419a5215ff949dd9c801120dd5d4f3527520b7c0
n4n5 <its.just.n4n5@gmail.com>
2026-08-11 15:02:30 +0200

    add

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -6,11 +6,6 @@ npm run build
 
 mkdir -p dist/animations/
 
-rustup update stable
-rustup default stable
-rustup set profile minimal
-rustup target add wasm32-unknown-unknown
-
 
 wget -qO- https://github.com/thedodd/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 
